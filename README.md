626403fe61feee211157b88a22bb395e34067706
John Vorsten <vorstenjohn@gmail.com>
2020-07-04 08:44:51 -0500

    Update documentation Update README Add examples

--- README.md
+++ README.md
@@ -1,7 +1,35 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul  3 20:15:50 2020
+# Dynamically rotate axis labels in matplotlib
 
-@author: z003vrzk
-"""
+3D plots have (3) axis'.  We have the ability to set tick positions and labels for each axis using the (axis3d)[https://matplotlib.org/api/_as_gen/mpl_toolkits.mplot3d.axis3d.Axis.html#mpl-toolkits-mplot3d-axis3d-axis] toolkit in matplot lib.  The API calls would go something like `axes.xaxis.set_xticklabels(['a','b']).  
 
+Fortunately, there is great control over how those labels are displayed in matplotlib.  We can set rotation, vertical alignment, and horizontal alignment relative to the viewers fixed screen coordinate system like : 
+
+```python
+fig3d = plt.figure(1)
+ax3d = fig3d.add_subplot(111, projection='3d')
+ax3d.set_title('Axes Title')
+ax3d.set_ylim(0,2)
+
+# Label X axis
+ax3d.set_xticks([1])
+ax3d.set_xticklabels(['Non-rotated Label\n"left,top"'])
+ax3d.xaxis.get_majorticklabels()[0].set_verticalalignment('top')
+ax3d.xaxis.get_majorticklabels()[0].set_horizontalalignment('left')
+# ax3d.xaxis.get_majorticklabels()[0].set_rotation(0)
+# Label y axis
+ax3d.set_yticks([1])
+ax3d.set_yticklabels(['Rotated Label'])
+ax3d.yaxis.get_majorticklabels()[0].set_rotation(45)
+
+# Label axis
+ax3d.set_xlabel('X Axis', labelpad=20)
+ax3d.set_ylabel('Y Axis', labelpad=20)
+```
+
+![Rotated Label](https://github.com/johnvorsten/mpl-rotate-axis-label/blob/master/Rotate_Example.png)
+
+But what if we want to continuously align a label to its respective axis? There is no way to anchor / redraw axis labels to continuously align them to an axis.  The default behavior to to set their rotation relative to the computer screen.  A solution is to use event signals to redraw axis labels.  Here is an example : 
+
+![Aligned Label](https://github.com/johnvorsten/mpl-rotate-axis-label/blob/master/Example.png)
+
+See the code for the full slot method and how to connect it to a figures 'draw-event'.
--- axis-label/axis-3d-relative.py
+++ axis-label/axis-3d-relative.py
@@ -2,6 +2,15 @@
 """
 Created on Fri Jul  3 20:16:22 2020
 
+See the matplotlib axis3d source for some of the projection code I took
+Specifically, I reused existing code to get the current dimensions of
+the displayed figure. Then, the dimensions and position of the Axis within
+the figure is found.  A projection of the Axis onto the users screen
+is found, and from there the rotation of the axis with respect to the users
+horizontal is calculated
+
+See https://matplotlib.org/_modules/mpl_toolkits/mplot3d/axis3d.html
+
 @author: z003vrzk
 """
 # Python imports
@@ -11,30 +20,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D, art3d, proj3d
 from mpl_toolkits.mplot3d.axis3d import get_flip_min_max
-from scipy import interpolate, integrate
-import pandas as pd
 
 
 #%%
 
 class RotateTickLabel:
     def __init__(self, axis, axes):
+        """
+        Rotate a set of axis labels relative to the axis
+        Hold state information (axis) to define which axis ticklabels should
+        be redrawn
+        inputs
+        -------
+        axis : (mpl_toolkits.mplot3d.axis3d.Axis) This is the Axis object
+            you want to rotate tick labels to. It is NOT the AXES object -
+            see the plot example for what to pass
+        axes : (mpl_toolkits.mplot3d.axes3d.Axes3D) This is the Axes object
+            that is probably a subplot of a figure. See the plot
+            example below for arguments to pass"""
         self.axis = axis
         self.axes = axes
         self.cid = axes.figure.canvas.mpl_connect('draw_event', self)
 
     def __call__(self, event):
-        print('Draw Event', event)
+        print('Draw Event', event) # TODO Remove when used
         self.set_axis_label_rotate(event)
 
     def set_axis_label_rotate(self, event):
+        """Rotate a set of axis labels relative to the axis
+        inputs
+        -------
+        event : (matplotlib.backend_bases.DrawEvent)
+        https://matplotlib.org/3.2.2/api/backend_bases_api.html#matplotlib.backend_bases.DrawEvent"""
 
         # Setup
         renderer = event.renderer
-        axes = self.axes
-        axis = self.axis # Axis... not the plot axes
-
-        # renderer.open_group('axis3d')
+        axes = self.axes # Axes - includes axes.xaxis and axes.yaxis
+        axis = self.axis # Axis
 
         info = axis._axinfo
         mins, maxs, centers, deltas, tc, highs = axis._get_coord_info(renderer)
@@ -52,6 +74,12 @@ class RotateTickLabel:
         pep = proj3d.proj_trans_points([edgep1, edgep2], renderer.M)
 
         # Draw labels
+        # The transAxes transform is used because the Text object
+        # rotates the text relative to the display coordinate system.
+        # Therefore, if we want the labels to remain parallel to the
+        # axis regardless of the aspect ratio, we need to convert the
+        # edge points of the plane to display coordinates and calculate
+        # an angle from that.
         peparray = np.asanyarray(pep)
         dx, dy = (axes.axes.transAxes.transform([peparray[0:2, 1]]) -
                   axes.axes.transAxes.transform([peparray[0:2, 0]]))[0]
@@ -66,6 +94,14 @@ class RotateTickLabel:
 
 
 def rotate_tick_label(event):
+    """This is a simple method to rotate only one group of axis labels
+    relative to the axis. It does not have the abaility to hold state
+    An option is to use a factor function to hold state, but this is a
+    simple example that works
+    inputs
+    -------
+    event : (matplotlib.backend_bases.DrawEvent)
+    https://matplotlib.org/3.2.2/api/backend_bases_api.html#matplotlib.backend_bases.DrawEvent"""
 
     # Setup
     renderer = event.renderer
@@ -94,9 +130,9 @@ def rotate_tick_label(event):
               axes.axes.transAxes.transform([peparray[0:2, 0]]))[0]
 
     # Rotate label
-    tick_label = axes.yaxis.get_majorticklabels()[0] # TODO Loop through later
-    angle = art3d._norm_text_angle(np.rad2deg(np.arctan2(dy, dx)))
-    tick_label.set_rotation(angle)
+    for tick_label in axes.yaxis.get_majorticklabels():
+        angle = art3d._norm_text_angle(np.rad2deg(np.arctan2(dy, dx)))
+        tick_label.set_rotation(angle)
 
     return None
 
@@ -161,3 +197,29 @@ def plot_integral():
 
 if __name__ == '__main__':
     plot_integral()
+
+
+#%% Play
+
+if __name__ == '__DontExecute':
+    fig3d = plt.figure(1)
+    ax3d = fig3d.add_subplot(111, projection='3d')
+    ax3d.set_title('Axes Title')
+    ax3d.set_xlim(0,5)
+    ax3d.set_ylim(0,5)
+
+    # Label X axis
+    ax3d.set_xticks([1])
+    ax3d.set_xticklabels(['Non-rotated Label\n"left,top"'])
+    ax3d.xaxis.get_majorticklabels()[0].set_verticalalignment('top')
+    ax3d.xaxis.get_majorticklabels()[0].set_horizontalalignment('left')
+    # ax3d.xaxis.get_majorticklabels()[0].set_rotation(0)
+    # Label y axis
+    ax3d.set_yticks([1])
+    ax3d.set_yticklabels(['Rotated Label'])
+    ax3d.yaxis.get_majorticklabels()[0].set_rotation(45)
+
+    # Label axis
+    ax3d.set_xlabel('X Axis', labelpad=20)
+    ax3d.set_ylabel('Y Axis', labelpad=20)
+    ax3d.set_zlabel('Z Axis', labelpad=20)
